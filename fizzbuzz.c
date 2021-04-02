1aa4c891c9a0d90314b4851d693652abb516b3a3
Gabriel Vinícius Heisler <61278049+GabrielHeisler@users.noreply.github.com>
2021-04-02 14:04:50 -0300

    Update fizzbuzz.c

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -11,14 +11,14 @@ bool mult5(int i){
 
 int main(){
     for(int i=1;i<=100;i++){
-        if(mult3(i)&&!mult5(i)){
+        if(mult5(i)&&mult3(i)){
+            printf("FizzBuzz\n");
+        }
+        else if(mult3(i)){
             printf("Fizz\n");
         }
-        else if(mult5(i)&&!mult3(i)){
+        else if(mult5(i)){
             printf("Buzz\n");
-        }
-        else if(mult5(i)&&mult3(i)){
-            printf("FizzBuzz\n");
         }
         else{
             printf("%d\n", i);
